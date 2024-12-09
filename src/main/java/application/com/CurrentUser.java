86275bc2d6e63f1200f37af0e33423d5f724c173
Fentomi <fentomi02@mail.ru>
2024-12-09 12:42:54 +0300

    Добавляет новые поля для пользователя внутри сессии.

--- src/main/java/application/com/CurrentUser.java
+++ src/main/java/application/com/CurrentUser.java
@@ -1,7 +1,15 @@
 package application.com;
 
 public class CurrentUser {
+    public static Integer userId;
     public static String userLogin;
     public static String userPassword;
-    public static Integer userId;
+
+    public static Integer personId;
+    public static String personName;
+    public static String personSurname;
+    public static String personLastname;
+
+    public static Integer roleId;
+    public static String roleName;
 }
