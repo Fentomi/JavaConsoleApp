b0282c34bd0807f5c1058175295bdf2551e9ee9c
Fentomi <fentomi02@mail.ru>
2024-12-14 00:29:03 +0300

    Добавил блоки try/catch в DatabaseController, чтобы убрать из функций throws.

--- src/DatabaseController.java
+++ src/DatabaseController.java
@@ -11,31 +11,59 @@ class DBSettings {
 public class DatabaseController {
     private Statement statement;
 
-    public ResultSet select(String sqlCommand) throws SQLException, ClassNotFoundException {
-        initConnect();
-        return statement.executeQuery(sqlCommand);
-    }
-    public void insert(String sqlCommand) throws SQLException, ClassNotFoundException {
-        initConnect();
-        statement.executeQuery(sqlCommand);
-    }
-    public void delete(String sqlCommand) throws SQLException, ClassNotFoundException {
-        initConnect();
-        statement.executeQuery(sqlCommand);
-    }
-    public ResultSet put(String sqlCommand) throws SQLException, ClassNotFoundException {
-        initConnect();
-        return statement.executeQuery(sqlCommand);
-    }
-    private void initConnect() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
+    private void initConnect() {
+        try {
+            Class.forName("org.postgresql.Driver");
 
-        Properties authorization = new Properties();
-        authorization.put("user", DBSettings.DB_LOGIN);
-        authorization.put("password", DBSettings.DB_PASSWORD);
-        Connection connection = DriverManager.getConnection(DBSettings.DB_URL, authorization);
+            Properties authorization = new Properties();
+            authorization.put("user", DBSettings.DB_LOGIN);
+            authorization.put("password", DBSettings.DB_PASSWORD);
+            Connection connection = DriverManager.getConnection(DBSettings.DB_URL, authorization);
 
-        statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
-                ResultSet.CONCUR_UPDATABLE);
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void closeConnect() {
+        try {
+            statement.close();
+        } catch (Exception _) { }
+    }
+
+    public ResultSet select(String sqlCommand) {
+        try {
+            initConnect();
+            ResultSet table = statement.executeQuery(sqlCommand);
+            closeConnect();
+            return table;
+        } catch (Exception _) {
+            return null;
+        }
+    }
+    public void insert(String sqlCommand) {
+        try {
+            initConnect();
+            statement.executeQuery(sqlCommand);
+            closeConnect();
+        } catch (Exception _) { }
+    }
+    public void delete(String sqlCommand) {
+        try {
+            initConnect();
+            statement.executeQuery(sqlCommand);
+            closeConnect();
+        } catch (Exception _) { }
+    }
+    public ResultSet put(String sqlCommand) {
+        try {
+            initConnect();
+            ResultSet table = statement.executeQuery(sqlCommand);
+            closeConnect();
+            return table;
+        } catch (Exception _) {
+            return null;
+        }
     }
 }
